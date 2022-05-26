d66cd614a10cdeaccdf678f1857f1bbada95cdad
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-05-26 14:34:51 -0300

    Initial commit

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    Vector3 posicion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicion = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "DeathWall")
+        {
+            transform.position = posicion;
+        }    
+    }
+
+    void OnCollisionExit()
+    {
+        transform.position = posicion;
+    }
+}
